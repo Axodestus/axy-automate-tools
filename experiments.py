05e4dcbd5b77bd4aaab4ddb64e40de3866c56685
Axodestus <axodestus@yandex.ru>
2022-06-07 11:40:50 +0300

    selection sort

--- experiments.py
+++ experiments.py
@@ -1,8 +1,26 @@
 import random
+import sys
+import os
+import time
 
 
-def main():
-    print("hello world")
+
+def find_smallest(data):
+    smallest = data[0]
+    smallest_index = 0
+    for i in range(1, len(data)): 
+        if data[i] < smallest:
+            smallest = data[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selection_sort(data):
+    new_data = []
+    for i in range(len(data)):
+        smallest_index = find_smallest(data)
+        new_data.append(data.pop(smallest_index))
+    return new_data
 
 
 def binary_search(value):
@@ -11,9 +29,7 @@ def binary_search(value):
     first = 0
     last = len(data) - 1
     while first <= last:
-
-
-        middle = first + last // 2 - 1
+        middle = first + last // 2 - 1 # this is main line of algo
         guess = data[middle]
         if guess == value:
             return middle
@@ -24,5 +40,13 @@ def binary_search(value):
     return -1
 
 
-if __name__ == '__main__':
+def main():
+    print("This is experiments with some algo")
+    data = [random.randint(1, 100) for x in range(1, 100)]
+        
     print(binary_search(5))
+    print(selection_sort(data))
+
+
+if __name__ == '__main__':
+    main()
